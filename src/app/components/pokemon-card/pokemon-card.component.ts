984f35f90f8065f7dfa5bf4328ea6c53ad3a3db1
Hugo Vinícius <ugopessoa@gmail.com>
2024-01-02 13:16:29 -0300

    initialize pokemon-list

---
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.css']
+})
+export class PokemonCardComponent {
+  @Input() pokemon = '';
+
+  @Input() numero: number = 0;
+
+  getImagePokemon() {
+    
+  }
+}
